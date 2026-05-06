4a3f3b8af55ba47b048e2b7b149f75879bfa0b50
Vamsi Addanki <vamsiaddanki666@gmail.com>
2026-05-05 23:07:22 -0400

    scripts

--- genome_scripts/run-all-h100.sh
+++ genome_scripts/run-all-h100.sh
@@ -15,62 +15,62 @@ export TRAFFIC_DIR=${SCRIPT_DIR}/traffic/
 source ${SCRIPT_DIR}/../.venv/bin/activate
 
 ##############################################################
-# echo "Running Moor 0 0 0 experiment"
-# mkdir -p ${SCRIPT_DIR}/summary-moor-0-0-0
+echo "Running Moor 0 0 0 experiment"
+mkdir -p ${SCRIPT_DIR}/summary-moor-0-0-0
 
-# (vllm serve $MODEL \
-# 		--tensor-parallel-size 1 \
-# 		--data-parallel-size 8 \
-# 		--enable-expert-parallel \
-# 		--all2all-backend deepep_high_throughput \
-# 		--trust_remote_code \
-# 		--max_num_batched_tokens 2048 \
-# 		--api-server-count=1 \
-# 		--expert-affinity-routing-weight 1 \
-# 		--kv-block-prefix-routing-weight 0.5 \
-# 		--load-score-routing-weight 0.5 \
-# 		--enable-eplb \
-# 		--eplb-config '{"policy":"custom","use_async":true,"step_interval":30,"window_size":1000,"num_redundant_experts":0,"graph_ema_alpha":0.5}' \
-# 		--placement-callback-path ${PLACEMENT_PATH} \
-# 		--placement-callback-func compute_placement  > ${SCRIPT_DIR}/summary-moor-0-0-0/vllm-log.txt 2> ${SCRIPT_DIR}/summary-moor-0-0-0/vllm-log.txt) &
+(vllm serve $MODEL \
+		--tensor-parallel-size 1 \
+		--data-parallel-size 8 \
+		--enable-expert-parallel \
+		--all2all-backend deepep_high_throughput \
+		--trust_remote_code \
+		--max_num_batched_tokens 2048 \
+		--api-server-count=1 \
+		--expert-affinity-routing-weight 1 \
+		--kv-block-prefix-routing-weight 0.5 \
+		--load-score-routing-weight 0.5 \
+		--enable-eplb \
+		--eplb-config '{"policy":"custom","use_async":true,"step_interval":30,"window_size":1000,"num_redundant_experts":0,"graph_ema_alpha":0.5}' \
+		--placement-callback-path ${PLACEMENT_PATH} \
+		--placement-callback-func compute_placement  > ${SCRIPT_DIR}/summary-moor-0-0-0/vllm-log.txt 2> ${SCRIPT_DIR}/summary-moor-0-0-0/vllm-log.txt) &
 
-# sleep 90
-# cd ${SCRIPT_DIR}/online-inference/
-# python3 send-prompts.py 0 0 0 128 ${SCRIPT_DIR}/traces-moor-0-0-0/ ${SCRIPT_DIR}/traffic-moor-0-0-0/ ${SCRIPT_DIR}/summary-moor-0-0-0/ \
-# 	> ${SCRIPT_DIR}/summary-moor-0-0-0/all-results.txt 2> ${SCRIPT_DIR}/summary-moor-0-0-0/all-results.txt
-
-
-# # cleanup
-# for i in $(nvidia-smi --query-compute-apps=pid,process_name,used_memory --format=csv | awk '{print $1}' | awk -F ',' '{print $1}');do kill -9 $i;done
-
-# ##############################################################
-# echo "Running MoorVertex 0 0 0 experiment"
-# mkdir -p ${SCRIPT_DIR}/summary-moorvertex-0-0-0
-
-# (vllm serve $MODEL \
-# 		--tensor-parallel-size 1 \
-# 		--data-parallel-size 8 \
-# 		--enable-expert-parallel \
-# 		--all2all-backend deepep_high_throughput \
-# 		--trust_remote_code \
-# 		--max_num_batched_tokens 2048 \
-# 		--api-server-count=1 \
-# 		--expert-affinity-routing-weight 1 \
-# 		--kv-block-prefix-routing-weight 0.5 \
-# 		--load-score-routing-weight 0.5 \
-# 		--enable-eplb \
-# 		--eplb-config '{"policy":"custom","use_async":true,"step_interval":30,"window_size":1000,"num_redundant_experts":0,"graph_ema_alpha":0.5}' \
-# 		--placement-callback-path "${SCRIPT_DIR}/expert-placement/placement_fns_vweights.py" \
-# 		--placement-callback-func compute_placement  > ${SCRIPT_DIR}/summary-moorvertex-0-0-0/vllm-log.txt 2> ${SCRIPT_DIR}/summary-moorvertex-0-0-0/vllm-log.txt) &
-
-# sleep 90
-# cd ${SCRIPT_DIR}/online-inference/
-# python3 send-prompts.py 0 0 0 128 ${SCRIPT_DIR}/traces-moorvertex-0-0-0/ ${SCRIPT_DIR}/traffic-moorvertex-0-0-0/ ${SCRIPT_DIR}/summary-moorvertex-0-0-0/ \
-# 	> ${SCRIPT_DIR}/summary-moorvertex-0-0-0/all-results.txt 2> ${SCRIPT_DIR}/summary-moorvertex-0-0-0/all-results.txt
+sleep 90
+cd ${SCRIPT_DIR}/online-inference/
+python3 send-prompts.py 0 0 0 128 ${SCRIPT_DIR}/traces-moor-0-0-0/ ${SCRIPT_DIR}/traffic-moor-0-0-0/ ${SCRIPT_DIR}/summary-moor-0-0-0/ \
+	> ${SCRIPT_DIR}/summary-moor-0-0-0/all-results.txt 2> ${SCRIPT_DIR}/summary-moor-0-0-0/all-results.txt
 
 
-# # cleanup
-# for i in $(nvidia-smi --query-compute-apps=pid,process_name,used_memory --format=csv | awk '{print $1}' | awk -F ',' '{print $1}');do kill -9 $i;done
+# cleanup
+for i in $(nvidia-smi --query-compute-apps=pid,process_name,used_memory --format=csv | awk '{print $1}' | awk -F ',' '{print $1}');do kill -9 $i;done
+
+##############################################################
+echo "Running MoorVertex 0 0 0 experiment"
+mkdir -p ${SCRIPT_DIR}/summary-moorvertex-0-0-0
+
+(vllm serve $MODEL \
+		--tensor-parallel-size 1 \
+		--data-parallel-size 8 \
+		--enable-expert-parallel \
+		--all2all-backend deepep_high_throughput \
+		--trust_remote_code \
+		--max_num_batched_tokens 2048 \
+		--api-server-count=1 \
+		--expert-affinity-routing-weight 1 \
+		--kv-block-prefix-routing-weight 0.5 \
+		--load-score-routing-weight 0.5 \
+		--enable-eplb \
+		--eplb-config '{"policy":"custom","use_async":true,"step_interval":30,"window_size":1000,"num_redundant_experts":0,"graph_ema_alpha":0.5}' \
+		--placement-callback-path "${SCRIPT_DIR}/expert-placement/placement_fns_vweights.py" \
+		--placement-callback-func compute_placement  > ${SCRIPT_DIR}/summary-moorvertex-0-0-0/vllm-log.txt 2> ${SCRIPT_DIR}/summary-moorvertex-0-0-0/vllm-log.txt) &
+
+sleep 90
+cd ${SCRIPT_DIR}/online-inference/
+python3 send-prompts.py 0 0 0 128 ${SCRIPT_DIR}/traces-moorvertex-0-0-0/ ${SCRIPT_DIR}/traffic-moorvertex-0-0-0/ ${SCRIPT_DIR}/summary-moorvertex-0-0-0/ \
+	> ${SCRIPT_DIR}/summary-moorvertex-0-0-0/all-results.txt 2> ${SCRIPT_DIR}/summary-moorvertex-0-0-0/all-results.txt
+
+
+# cleanup
+for i in $(nvidia-smi --query-compute-apps=pid,process_name,used_memory --format=csv | awk '{print $1}' | awk -F ',' '{print $1}');do kill -9 $i;done
 
 ##############################################################
 echo "Running EPLB experiment"
